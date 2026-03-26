def0bcee550ec80848024d0f2006077f9b5165de
absurdoliver <166875516+absurd-oliver@users.noreply.github.com>
2026-03-26 10:35:34 -0400

    Refactor fact display and add page load event
    Refactor displayFact function to simplify DOM manipulation and update the way the fact is displayed. Add event listener for DOMContentLoaded to fetch a fact on page load.

--- script.js
+++ script.js
@@ -1,62 +1,29 @@
-
 // API: https://uselessfacts.jsph.pl/api/v2/facts/random?language=en
 
-// return-object fields: text, source, source_url, language
-
+// returns object -> fields: text, source, source_url, language
 
 async function getUselessFact() {
   try {
-    
     const response = await fetch('https://uselessfacts.jsph.pl/api/v2/facts/random?language=en');
-    
     const data = await response.json();
-    
     displayFact(data);
-    
   } catch (error) {
     console.error('Error fetching fact:', error);
   }
 }
 
 function displayFact(obj){
-  
   const newSpan = document.createElement('p');
   newSpan.id = 'funFactSpan';
-  
-  const newText = document.createElement('p');
-  newText.id = 'funFactText';
-  newText.innerText = `${obj.text}\n by:`;
-  
-  const newAnchor = document.createElement('a');
-  newAnchor.id = 'funFactSource';
-  newAnchor.innerText = `${obj.source}`;
-  newAnchor.href = `${obj.source_url}`;
-
-  
   document.body.appendChild(newSpan);
-  document.getElementById('funFactSpan').appendChild(newText);
-  document.getElementById('funFactSpan').appendChild(newAnchor);
-  
+  newSpan.style = 'white-space: pre-line;';
+  newSpan.innerHTML = `${obj.text} \n source: <a href='${obj.source_url}'>${obj.source}</a>`;
 }
 
+window.addEventListener('DOMContentLoaded', getUselessFact);
 
-const test = {
-  'text': 'testing',
-  'source': 'i made it tf up',
-  'source_url': 'https://i_made_it_tf_up.com',
-  'language': 'en'
-};
-
-// displayFact(test);
-
-
-
-getUselessFact();
-
-
-
-
-
+const footer = document.querySelector('footer');
+footer.addEventListener('click', () => {  window.location.refresh; });
 
 
 
