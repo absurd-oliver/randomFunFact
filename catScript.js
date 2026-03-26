a7604ea522190c860e5818a27ef6fe5421176720
absurdoliver <166875516+absurd-oliver@users.noreply.github.com>
2026-03-26 13:48:13 -0400

    Add files via upload

---
+++ catScript.js
@@ -0,0 +1,33 @@
+// API: https://cataas.com/cat
+
+// returns object -> fields: Fact
+
+async function getCat() {
+  try {
+    await fetch('https://cataas.com/cat')
+    .then (response => response.blob())
+    .then(blob => {
+        const objectURL = URL.createObjectURL(blob);
+        displayFact(objectURL);
+    });
+  } catch (error) {
+    console.error('Error fetching fact:', error);
+  }
+}
+
+function displayFact(obj){
+  const newImg = document.createElement('img');
+  newImg.id = 'catImg';
+  newImg.src = `${obj}`;
+  newImg.style = 'margin-top: 5vh;';
+  document.body.appendChild(newImg);
+}
+
+window.addEventListener('DOMContentLoaded', getCat);
+
+const footer = document.querySelector('footer');
+footer.addEventListener('click', () => {  window.location.reload(); });
+
+
+
+
